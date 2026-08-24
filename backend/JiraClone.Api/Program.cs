57812d483ece995b9df5656261d9e973cf096d99
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-08-25 02:08:23 +0530

    refactor(api): register issue application service

--- backend/JiraClone.Api/Program.cs
+++ backend/JiraClone.Api/Program.cs
@@ -1,8 +1,10 @@
 using JiraClone.Api.Data;
+using JiraClone.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<JiraDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=jira.db"));
+builder.Services.AddScoped<IssueApplicationService>();
 builder.Services.AddCors(o => o.AddPolicy("frontend", p => p.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
