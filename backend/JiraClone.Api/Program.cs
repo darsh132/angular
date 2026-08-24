ddcd24efc1e27bd245cc1739f81e08ca2b51ff07
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-08-25 02:25:32 +0530

    fix: register sprint application service

--- backend/JiraClone.Api/Program.cs
+++ backend/JiraClone.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<JiraDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=jira.db"));
 builder.Services.AddScoped<IssueApplicationService>();
+builder.Services.AddScoped<SprintApplicationService>();
 builder.Services.AddCors(o => o.AddPolicy("frontend", p => p.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
 builder.Services.AddProblemDetails(options =>
 {
