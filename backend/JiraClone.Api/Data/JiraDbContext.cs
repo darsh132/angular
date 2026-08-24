2875e25fa7f9ad9fb5472b4dda8882261804f5bf
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-08-25 02:07:57 +0530

    feat(data): map issue activity and enum persistence

--- backend/JiraClone.Api/Data/JiraDbContext.cs
+++ backend/JiraClone.Api/Data/JiraDbContext.cs
@@ -10,31 +10,24 @@ public sealed class JiraDbContext(DbContextOptions<JiraDbContext> options) : DbC
     public DbSet<Sprint> Sprints => Set<Sprint>();
     public DbSet<Issue> Issues => Set<Issue>();
     public DbSet<IssueComment> IssueComments => Set<IssueComment>();
+    public DbSet<IssueActivity> IssueActivities => Set<IssueActivity>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique();
         modelBuilder.Entity<Project>().HasIndex(x => x.Key).IsUnique();
         modelBuilder.Entity<Issue>().HasIndex(x => new { x.ProjectId, x.Number }).IsUnique();
+        modelBuilder.Entity<Issue>().Property(x => x.Status).HasConversion<string>();
+        modelBuilder.Entity<Issue>().Property(x => x.Priority).HasConversion<string>();
+        modelBuilder.Entity<Issue>().Property(x => x.Type).HasConversion<string>();
+        modelBuilder.Entity<IssueActivity>().Property(x => x.Type).HasConversion<string>();
 
-        modelBuilder.Entity<Issue>()
-            .HasOne(x => x.Project).WithMany(x => x.Issues)
-            .HasForeignKey(x => x.ProjectId).OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<Issue>()
-            .HasOne(x => x.Assignee).WithMany()
-            .HasForeignKey(x => x.AssigneeId).OnDelete(DeleteBehavior.SetNull);
-
-        modelBuilder.Entity<Issue>()
-            .HasOne(x => x.Sprint).WithMany(x => x.Issues)
-            .HasForeignKey(x => x.SprintId).OnDelete(DeleteBehavior.SetNull);
-
-        modelBuilder.Entity<IssueComment>()
-            .HasOne(x => x.Issue).WithMany(x => x.Comments)
-            .HasForeignKey(x => x.IssueId).OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<IssueComment>()
-            .HasOne(x => x.Author).WithMany()
-            .HasForeignKey(x => x.AuthorId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Issue>().HasOne(x => x.Project).WithMany(x => x.Issues).HasForeignKey(x => x.ProjectId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<Issue>().HasOne(x => x.Assignee).WithMany().HasForeignKey(x => x.AssigneeId).OnDelete(DeleteBehavior.SetNull);
+        modelBuilder.Entity<Issue>().HasOne(x => x.Sprint).WithMany(x => x.Issues).HasForeignKey(x => x.SprintId).OnDelete(DeleteBehavior.SetNull);
+        modelBuilder.Entity<IssueComment>().HasOne(x => x.Issue).WithMany(x => x.Comments).HasForeignKey(x => x.IssueId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<IssueComment>().HasOne(x => x.Author).WithMany().HasForeignKey(x => x.AuthorId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<IssueActivity>().HasOne(x => x.Issue).WithMany(x => x.Activities).HasForeignKey(x => x.IssueId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<IssueActivity>().HasOne(x => x.Actor).WithMany().HasForeignKey(x => x.ActorId).OnDelete(DeleteBehavior.Restrict);
     }
 }
